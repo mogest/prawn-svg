a2110d34367eb46e8153522420f1ba52c290ebf9
Nando Vieira <fnando.vieira@gmail.com>
2013-08-16 18:27:00 -0300

    Create namespace before requiring files.

--- lib/prawn-svg.rb
+++ lib/prawn-svg.rb
@@ -1,13 +1,17 @@
-require 'prawn'
-require 'prawn/svg/color'
-require 'prawn/svg/extension'
-require 'prawn/svg/interface'
-require 'prawn/svg/font'
-require 'prawn/svg/document'
-require 'prawn/svg/element'
-require 'prawn/svg/parser'
-require 'prawn/svg/parser/path'
-require 'prawn/svg/parser/text'
-require 'prawn/svg/parser/image'
+module Prawn
+  module Svg
+    require 'prawn'
+    require 'prawn/svg/color'
+    require 'prawn/svg/extension'
+    require 'prawn/svg/interface'
+    require 'prawn/svg/font'
+    require 'prawn/svg/document'
+    require 'prawn/svg/element'
+    require 'prawn/svg/parser'
+    require 'prawn/svg/parser/path'
+    require 'prawn/svg/parser/text'
+    require 'prawn/svg/parser/image'
+  end
+end
 
 Prawn::Document.extensions << Prawn::Svg::Extension
