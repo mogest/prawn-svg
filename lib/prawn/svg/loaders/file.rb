f43bf2a6056eec2e4de32aa69c0349648c7720c4
Roger Nesbitt <roger@seriousorange.com>
2016-03-28 13:04:07 +1300

    Improvements to file:// URL parsing
    * Support Windows file:///x:/path URLs
    * URL decode the path before using it
    * Ensure root_path is not an empty string

--- lib/prawn/svg/loaders/file.rb
+++ lib/prawn/svg/loaders/file.rb
@@ -1,8 +1,33 @@
+#
+# Load a file from disk.
+#
+# WINDOWS
+# =======
+# Windows is supported, but must use URLs in the modern structure like:
+#   file:///x:/path/to/the/file.png
+# or as a relative path:
+#   directory/file.png
+# or as an absolute path from the current drive:
+#   /path/to/the/file.png
+#
+# Ruby's URI parser does not like backslashes, nor can it handle filenames as URLs starting
+# with a drive letter as it thinks you're giving it a scheme.
+#
+# URL ENCODING
+# ============
+# This module assumes the URL that is passed in has been URL-encoded.  If for some reason
+# you're passing in a filename that hasn't been taken from an XML document's attribute,
+# you will want to URL encode it before you pass it in.
+#
 module Prawn::SVG::Loaders
   class File
     attr_reader :root_path
 
     def initialize(root_path)
+      if root_path.empty?
+        raise ArgumentError, "An empty string is not a valid root path.  Use '.' if you want the current working directory."
+      end
+
       @root_path = ::File.expand_path(root_path)
 
       raise ArgumentError, "#{root_path} is not a directory" unless Dir.exist?(@root_path)
@@ -12,19 +37,20 @@ module Prawn::SVG::Loaders
       uri = build_uri(url)
 
       if uri && uri.scheme.nil? && uri.path
-        path = build_absolute_path(uri.path)
-        load_file(path)
+        load_file(uri.path)
 
       elsif uri && uri.scheme == 'file'
         assert_valid_file_uri!(uri)
-        load_file(uri.path)
+        path = windows? ? fix_windows_path(uri.path) : uri.path
+        load_file(path)
       end
     end
 
     private
 
     def load_file(path)
-      path = ::File.expand_path(path)
+      path = URI.decode(path)
+      path = build_absolute_and_expand_path(path)
       assert_valid_path!(path)
       assert_file_exists!(path)
       IO.read(path)
@@ -38,17 +64,17 @@ module Prawn::SVG::Loaders
     end
 
     def assert_valid_path!(path)
-      if !path.start_with?("#{root_path}/")
+      # TODO : case sensitive comparison, but it's going to be a bit of a headache
+      # making it dependent on whether the file system is case sensitive or not.
+      # Leaving it like this until it's a problem for someone.
+
+      if !path.start_with?("#{root_path}#{::File::SEPARATOR}")
         raise Prawn::SVG::UrlLoader::Error, "file path is not inside the root path of #{root_path}"
       end
     end
 
-    def build_absolute_path(path)
-      if path[0] == "/"
-        path
-      else
-        "#{root_path}/#{path}" 
-      end
+    def build_absolute_and_expand_path(path)
+      ::File.expand_path(path, root_path)
     end
 
     def assert_valid_file_uri!(uri)
@@ -61,6 +87,18 @@ module Prawn::SVG::Loaders
       if !::File.exist?(path)
         raise Prawn::SVG::UrlLoader::Error, "File #{path} does not exist"
       end
+    end
+
+    def fix_windows_path(path)
+      if matches = path.match(%r(\A/[a-z]:/)i)
+        path[1..-1]
+      else
+        path
+      end
+    end
+
+    def windows?
+      !!(RbConfig::CONFIG['host_os'] =~ /mswin|msys|mingw|cygwin|bccwin|wince|emc/)
     end
   end
 end
--- spec/prawn/svg/loaders/file_spec.rb
+++ spec/prawn/svg/loaders/file_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 RSpec.describe Prawn::SVG::Loaders::File do
   let(:root_path) { "." }
+  let(:fake_root_path) { "/some" }
 
-  subject { Prawn::SVG::Loaders::File.new(root_path).from_url(url) }
+  let(:file_loader) { Prawn::SVG::Loaders::File.new(root_path) }
+  subject { file_loader.from_url(url) }
 
   context "when an invalid path is supplied" do
     let(:root_path) { "/does/not/exist" }
@@ -14,15 +16,15 @@ RSpec.describe Prawn::SVG::Loaders::File do
   end
 
   context "when a relative path is supplied" do
-    let(:url) { "some/relative/./path" }
+    let(:url) { "relative/./path" }
 
     it "loads the file" do
-      expect(File).to receive(:expand_path).with(".").and_return("/our/root/path")
-      expect(File).to receive(:expand_path).with("/our/root/path/some/relative/./path").and_return("/our/root/path/some/relative/path")
+      expect(File).to receive(:expand_path).with(".").and_return(fake_root_path)
+      expect(File).to receive(:expand_path).with("relative/./path", fake_root_path).and_return("#{fake_root_path}/relative/path")
 
-      expect(Dir).to receive(:exist?).with("/our/root/path").and_return(true)
-      expect(File).to receive(:exist?).with("/our/root/path/some/relative/path").and_return(true)
-      expect(IO).to receive(:read).with("/our/root/path/some/relative/path").and_return("data")
+      expect(Dir).to receive(:exist?).with(fake_root_path).and_return(true)
+      expect(File).to receive(:exist?).with("#{fake_root_path}/relative/path").and_return(true)
+      expect(IO).to receive(:read).with("#{fake_root_path}/relative/path").and_return("data")
 
       expect(subject).to eq 'data'
     end
@@ -32,10 +34,10 @@ RSpec.describe Prawn::SVG::Loaders::File do
     let(:url) { "/some/absolute/./path" }
 
     it "loads the file" do
-      expect(File).to receive(:expand_path).with(".").and_return("/some")
-      expect(File).to receive(:expand_path).with(url).and_return("/some/absolute/path")
+      expect(File).to receive(:expand_path).with(".").and_return(fake_root_path)
+      expect(File).to receive(:expand_path).with(url, fake_root_path).and_return("/some/absolute/path")
 
-      expect(Dir).to receive(:exist?).with("/some").and_return(true)
+      expect(Dir).to receive(:exist?).with(fake_root_path).and_return(true)
       expect(File).to receive(:exist?).with("/some/absolute/path").and_return(true)
       expect(IO).to receive(:read).with("/some/absolute/path").and_return("data")
 
@@ -44,28 +46,28 @@ RSpec.describe Prawn::SVG::Loaders::File do
   end
 
   context "when an absolute path with file scheme is supplied" do
-    let(:url) { "file:///some/absolute/./path" }
+    let(:url) { "file:///some/absolute/./path%20name" }
 
     it "loads the file" do
-      expect(File).to receive(:expand_path).with(".").and_return("/some")
-      expect(File).to receive(:expand_path).with("/some/absolute/./path").and_return("/some/absolute/path")
+      expect(File).to receive(:expand_path).with(".").and_return(fake_root_path)
+      expect(File).to receive(:expand_path).with("/some/absolute/./path name", fake_root_path).and_return("/some/absolute/path name")
 
-      expect(Dir).to receive(:exist?).with("/some").and_return(true)
-      expect(File).to receive(:exist?).with("/some/absolute/path").and_return(true)
-      expect(IO).to receive(:read).with("/some/absolute/path").and_return("data")
+      expect(Dir).to receive(:exist?).with(fake_root_path).and_return(true)
+      expect(File).to receive(:exist?).with("/some/absolute/path name").and_return(true)
+      expect(IO).to receive(:read).with("/some/absolute/path name").and_return("data")
 
       expect(subject).to eq 'data'
     end
   end
 
   context "when a path outside of our root is specified" do
-    let(:url) { "/some/absolute/./path" }
+    let(:url) { "/other/absolute/./path" }
 
     it "raises" do
-      expect(File).to receive(:expand_path).with(".").and_return("/other")
-      expect(File).to receive(:expand_path).with(url).and_return("/some/absolute/path")
+      expect(File).to receive(:expand_path).with(".").and_return(fake_root_path)
+      expect(File).to receive(:expand_path).with(url, fake_root_path).and_return("/other/absolute/path")
 
-      expect(Dir).to receive(:exist?).with("/other").and_return(true)
+      expect(Dir).to receive(:exist?).with(fake_root_path).and_return(true)
 
       expect { subject }.to raise_error Prawn::SVG::UrlLoader::Error, /not inside the root path/
     end
@@ -75,10 +77,27 @@ RSpec.describe Prawn::SVG::Loaders::File do
     let(:url) { "file://somewhere/somefile" }
 
     it "raises" do
-      expect(File).to receive(:expand_path).with(".").and_return("/other")
-      expect(Dir).to receive(:exist?).with("/other").and_return(true)
+      expect(File).to receive(:expand_path).with(".").and_return(fake_root_path)
+      expect(Dir).to receive(:exist?).with(fake_root_path).and_return(true)
 
       expect { subject }.to raise_error Prawn::SVG::UrlLoader::Error, /with a host/
+    end
+  end
+
+  context "when we're running on Windows" do
+    let(:url) { "file:///c:/path/to/file.png" }
+    let(:fake_root_path) { "c:/full" }
+
+    it "automatically fixes up URI's misparsing of Windows file paths and loads the file" do
+      expect(File).to receive(:expand_path).with(".").and_return(fake_root_path)
+      expect(File).to receive(:expand_path).with("c:/path/to/file.png", fake_root_path).and_return("c:/full/path/to/file.png")
+
+      expect(Dir).to receive(:exist?).with(fake_root_path).and_return(true)
+      expect(File).to receive(:exist?).with("c:/full/path/to/file.png").and_return(true)
+      expect(IO).to receive(:read).with("c:/full/path/to/file.png").and_return("data")
+
+      allow(file_loader).to receive(:windows?).and_return true
+      expect(subject).to eq 'data'
     end
   end
 end
