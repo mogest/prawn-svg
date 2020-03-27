33ffc14ae88d92e686d55d16227ad53057f57fb9
Roger Nesbitt <roger@seriousorange.com>
2020-03-28 11:58:55 +1300

    Respect the fill property on the root <svg> element
    Fixes #120.

--- lib/prawn/svg/elements/root.rb
+++ lib/prawn/svg/elements/root.rb
@@ -8,7 +8,10 @@ class Prawn::SVG::Elements::Root < Prawn::SVG::Elements::Base
   end
 
   def apply
-    add_call 'fill_color', '000000'
+    if [nil, 'inherit', 'none', 'currentColor'].include?(properties.fill)
+      add_call 'fill_color', '000000'
+    end
+
     add_call 'transformation_matrix', @document.sizing.x_scale, 0, 0, @document.sizing.y_scale, 0, 0
     add_call 'transformation_matrix', 1, 0, 0, 1, -@document.sizing.x_offset, @document.sizing.y_offset
   end
